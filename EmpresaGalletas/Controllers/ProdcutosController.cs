db8aedc317caf0a10d7c4357f64a1d71d0c83f3c
Aldedier <aldedier.martinez2851@correo.policia.gov.co>
2018-05-02 09:32:54 -0500

    Cambio a productos

---
+++ EmpresaGalletas/Controllers/ProdcutosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmpresaGalletas.Controllers
+{
+    public class ProdcutosController : Controller
+    {
+        // GET: Prodcutos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmpresaGalletas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmpresaGalletas.MvcApplication" Language="C#" %>
