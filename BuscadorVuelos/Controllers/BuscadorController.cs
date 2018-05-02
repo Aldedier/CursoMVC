8fcca7d6800ddc9c7f4f3feb5e69137b284cd043
Aldedier <aldedier.martinez2851@correo.policia.gov.co>
2018-05-02 09:22:46 -0500

    Inicio proyecto MVC

---
+++ BuscadorVuelos/Controllers/BuscadorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BuscadorVuelos.Controllers
+{
+    public class BuscadorController : Controller
+    {
+        // GET: Buscador
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
